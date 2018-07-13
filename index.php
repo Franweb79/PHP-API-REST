ff84291146ea58682d27a035acb6a1b93585e3a5
Franweb79 <franprietogutweb@gmail.com>
2018-07-13 23:57:11 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,193 @@
+<?php
+
+
+    require_once 'models/productosPDO.php';
+
+    require_once 'vendor/autoload.php';
+
+    header('Access-Control-Allow-Origin: *');
+    header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+    header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+    $method = $_SERVER['REQUEST_METHOD'];
+    if($method == "OPTIONS") {
+            die();
+    }
+
+    
+
+    $app=new \Slim\Slim();
+
+    
+   // $conn=new ConnectionDataBasePDO();
+
+   $objProducto=new  ProductosPDO();
+
+
+
+
+    $app->get("/prueba",function()  use($app){
+
+    
+
+      
+        echo "hola mundo desde slim";
+
+        
+    });
+
+    $app->get("/prueba2",function()  use($app){
+
+        echo "p2";
+     });
+
+    $app->get("/",function()  use($app){
+
+        echo "hola index";
+     });
+
+
+
+
+     //PATH TO INSERT PRODUCTS
+
+     $app->post("/productos",function() use($app,$objProducto){
+
+        $json=$app->request->post("json");
+        $dataDecoded=json_decode($json,true);//el true hace que se nos convierta de un objeto a un array
+
+        
+        if(!isset($dataDecoded['nombre']))
+        {
+            $dataDecoded['nombre']=NULL;
+        }
+        
+        if(!isset($dataDecoded['descripcion']))
+        {
+            $dataDecoded['descripcion']="";
+        }
+
+        
+        if(!isset($dataDecoded['precio']))
+        {
+            $dataDecoded['precio']=NULL;
+        }
+
+
+        if(!isset($dataDecoded['imagen']))
+        {
+            $dataDecoded['imagen']=NULL;
+        }
+
+       $objProducto->insertProduct($dataDecoded["nombre"],$dataDecoded["descripcion"],$dataDecoded["precio"], $dataDecoded['imagen']);
+
+
+    
+     });
+
+     //PATH TO UPDATE PRODUCTS
+
+     $app->post("/productos/:id_producto",function($id_producto) use($app,$objProducto){
+
+        $newJSONData=$app->request->post("json");
+        
+        $objProducto->updateProductById($id_producto,$newJSONData);
+
+
+     });
+
+
+     //PATH TO GET PRODUCTS
+
+     $app->get("/productos",function() use($app,$objProducto){
+
+
+        $resultados=$objProducto->getProducts();
+
+        $resjson=json_encode($resultados);
+
+      
+        echo $resjson;
+     });
+
+
+
+
+     //PATH TP GET ONLY ONE PRODUCT
+
+     $app->get("/productos/:id_producto",function($id_producto) use($app,$objProducto){
+
+      /*in slim 2, we can embed route parameters like so:
+
+      https://docs.slimframework.com/routing/params/
+
+
+      <?php
+            $app = new \Slim\Slim();
+            $app->get('/books/:one/:two', function ($one, $two) {
+                echo "The first parameter is " . $one;
+                echo "The second parameter is " . $two;
+        
+            }); 
+        ?>
+
+      */
+
+        
+        $resultados=$objProducto->getProductById($id_producto);
+
+        $resjson=json_encode($resultados);
+
+        echo $resjson;
+     });
+
+
+     /*PATH TO DELETE PRODUCTS. We won´t use DELETE HTTP verb because it could give configuration problems
+     with Apache, and in real life mostly GET and POST are only used*/
+
+     $app->get("/delete-productos/:id_producto",function($id_producto) use($app,$objProducto){
+
+           $objProducto->deleteProductById($id_producto);
+
+            
+        }
+
+     );
+
+     //path to upload images
+
+     $app->post("/upload-file/:id_producto",function($id_producto) use($app,$objProducto){
+
+        
+        
+        
+        //first, we upload it and we will pass the name to the database with the updateFile method
+        
+        
+        
+        
+        $imageData=$objProducto->uploadFile();
+
+       // echo $imageData["complete_name"];
+
+        $imageDataNameArray= Array(
+
+
+                                'imagen' => $imageData["complete_name"]
+                            );
+
+       
+        
+        $imageDataNameJSON=json_encode($imageDataNameArray);
+
+        $objProducto->updateProductById($id_producto,$imageDataNameJSON);
+
+
+
+     });
+
+     
+    $app->run();
+
+
+?>
---
+++ models/productosPDO.php
@@ -0,0 +1,431 @@
+
+<?php
+
+require_once('ConnectPDO.php');
+
+
+class ProductosPDO{
+
+    //public $id_producto;
+
+    //better make then private and access though setters and getters
+    public $nombre;
+    public $descripcion;
+    public $precio;
+    public $imagen;
+
+    function __construct()
+    {
+        $this->nombre=null;
+        $this->descripcion="";
+        $this->precio=null;
+        $this->imagen =null;
+    }
+
+    //LISTAR TODOS
+
+    public function getProducts()
+    {
+
+        $conexion=new ConnectionDataBasePDO();
+
+        $conexion->openConnectionPDO();
+
+        $sql="SELECT * FROM productos";
+        
+        $sth=$conexion->connection->prepare($sql);
+
+        
+
+        //execute statement
+
+        $sth->execute();
+
+
+        // Set fetch mode to FETCH_ASSOC to return an array indexed by column name
+
+        $sth->setFetchMode(PDO::FETCH_ASSOC);
+
+       
+         /*fetchall() returns all rows; fetch() only one*/
+        $result=$sth->fetchall();
+
+
+        if($result ==null)
+        {
+           
+            $result=Array(
+
+                'status' => 'error',
+                'responseCode' => 404,
+                'message'=>'No products yet!!!!'
+            );
+            
+            
+
+        }
+     
+
+        $conexion->closeConnectionPDO();
+
+
+        return $result;
+    }
+
+    //LISTAR UNO
+
+    public function getProductById($p_idProducto)
+    {
+        $conexion=new ConnectionDataBasePDO();
+
+        $conexion->openConnectionPDO();
+
+        $sql="SELECT * FROM productos WHERE id_producto=:id_producto";
+
+        //echo $sql;
+
+        $sth=$conexion->connection->prepare($sql);
+
+
+
+        $sth->bindParam(':id_producto',$p_idProducto);
+
+      
+        
+        //execute statement
+
+        $sth->execute();
+
+        $sth->setFetchMode(PDO::FETCH_ASSOC);
+
+        /*fetchall() returns all rows; fetch() only one*/
+        $result=$sth->fetch();
+        
+
+
+
+        if($result==null)
+        {
+          
+            $toShow=Array(
+
+                'status' => 'error',
+                'responseCode' => 404,
+                'message'=>'No product with this ID'
+            );
+           
+
+        }
+        else
+        {
+            
+            $toShow=Array(
+
+                'status' => 'success',
+                'responseCode' => 200,
+                'message'=>$result
+            );
+        }
+
+        $conexion->closeConnectionPDO();
+
+
+        return $toShow;
+
+    }
+
+    //ELIMINAR PRODUCTO
+
+    public function deleteProductById($p_idProducto)
+    {
+        try{
+
+
+
+            $conn=new ConnectionDataBasePDO();
+
+            $conn-> openConnectionPDO();
+
+           
+    
+    
+            $sql="DELETE FROM productos where id_producto=:id_producto";
+    
+            $sth=$conn->connection->prepare($sql);
+    
+            
+            $sth->bindParam(':id_producto',$p_idProducto);
+            
+            //execute statement
+           
+             $sth->execute();
+
+             /*I can check affected rows by the execute method with rowCount()
+
+             https://stackoverflow.com/questions/44305738/deleting-record-if-it-exists-in-php-pdo
+
+             */
+
+             $count = $sth->rowCount();// check affected rows using rowCount
+             if ($count > 0) {
+                 echo 'Success - The record has been deleted.';
+             } else {
+                 echo "No product with that ID, so could not delete";
+             }
+
+          
+             $conn->closeConnectionPDO();
+        }
+
+        catch(PDOException $e){
+    
+            echo "deletion failed: " . $e->getMessage();
+
+        }
+        
+    }
+
+
+
+
+    //METER PRODUCTO
+    public function insertProduct($p_nombre,$p_descripcion,$p_precio,$p_imagen)
+    {
+        try{
+
+
+
+                $conn=new ConnectionDataBasePDO();
+
+                $conn-> openConnectionPDO();
+        
+        
+                $sql="INSERT INTO productos (nombre,descripcion,precio,imagen) VALUES (:nombre,:descripcion,:precio,:imagen)";
+        
+                $sth=$conn->connection->prepare($sql);
+        
+                
+                $sth->bindParam(':nombre',$p_nombre);
+        
+                $sth->bindParam(':descripcion',$p_descripcion);
+        
+                $sth->bindParam(':precio',$p_precio);
+        
+                $sth->bindParam(':imagen',$p_imagen);
+        
+        
+            //execute statement
+    
+            if($sth->execute())//execute returns true if everithing is ok
+            {
+                $result=array(
+
+                    'status' => 'ok',
+                    'responseCode' => 200,
+                    'message'=>'successfully registered product'
+                );
+            }
+            else
+            {
+                $result=array(
+
+                    'status' => 'error',
+                    'responseCode' => 404,
+                    'message'=>'couldn´t create the product'
+                );
+            }
+    
+           
+           
+
+
+            echo json_encode($result);
+
+            
+    
+    
+    
+            $conn->closeConnectionPDO();
+        }
+
+        catch(PDOException $e){
+    
+            echo "insertion failed: " . $e->getMessage();
+
+        }
+        
+        
+    }
+
+    //ACTUALIZAR PRODUCTO
+
+    public function updateProductById($p_idProducto,$p_newJSONDataToInsert)
+    {
+        try{
+
+                //check if product exists
+           
+                $objProductoToCheckIfExistsThisOne=new ProductosPDO();
+
+                $resultOfCheckingIfProductExists=$objProductoToCheckIfExistsThisOne->getProductById($p_idProducto);
+
+           
+
+            
+                // $resultOfCheckingIfProductExists=json_encode($resultOfCheckingIfProductExists);
+
+                //can´t do json_encode and access it through php! php can´t do that! we read directly the array returned by getProductById
+            
+                if($resultOfCheckingIfProductExists['responseCode']==404)
+                {
+                    echo "no existe";
+                }
+                if($resultOfCheckingIfProductExists['responseCode']==200)
+                {
+                //if product exists, we will check if fields are empty on new data,
+                //then those fields on new data will be the ones from old data
+
+                    $jsonDataDecoded=json_decode($p_newJSONDataToInsert,true);
+                    
+                    if(!isset($jsonDataDecoded['nombre']))
+                    {
+
+                            $jsonDataDecoded['nombre']=$resultOfCheckingIfProductExists["message"]["nombre"];
+                    }
+
+                    if(!isset($jsonDataDecoded['descripcion']))
+                    {
+
+                            $jsonDataDecoded['descripcion']=$resultOfCheckingIfProductExists["message"]["descripcion"];
+                    }
+
+                    if(!isset($jsonDataDecoded['precio']))
+                    {
+
+                            $jsonDataDecoded['precio']=$resultOfCheckingIfProductExists["message"]["precio"];
+                    }
+
+                    if(!isset($jsonDataDecoded['imagen']))
+                    {
+
+                            $jsonDataDecoded['imagen']=$resultOfCheckingIfProductExists["message"]["imagen"];
+                    }
+
+                    //var_dump($jsonDataDecoded);
+
+                    /*now with all data properly set, we can update:*/
+
+                    $conn=new ConnectionDataBasePDO();
+
+                    $conn-> openConnectionPDO();
+
+                    $sql="UPDATE productos
+                    SET nombre=:nombre,
+                        descripcion=:descripcion,
+                        precio=:precio,
+                        imagen=:imagen
+                    WHERE id_producto=:id_producto;";
+
+                    
+                    $sth=$conn->connection->prepare($sql);
+
+                    //bind parameters to statement variables
+ 
+                    $sth->bindParam(':nombre', $jsonDataDecoded['nombre']);
+
+                    $sth->bindParam(':descripcion',$jsonDataDecoded['descripcion']);
+
+                    $sth->bindParam(':precio', $jsonDataDecoded['precio']);
+
+                    $sth->bindParam(':imagen',  $jsonDataDecoded['imagen']);
+
+                    $sth->bindParam(':id_producto',$p_idProducto);
+
+                    //execute statement
+ 
+                    //if everything is ok
+                    if($sth->execute())
+                    {
+                        /*we get it again with new data to show it*/
+                        
+                        $newProduct=$objProductoToCheckIfExistsThisOne->getProductById($p_idProducto);
+                        
+                        $toShow=Array(
+
+                            'status' => 'ok',
+                            'responseCode' => 200,
+                            'message'=>$newProduct["message"]
+                        );
+                    }
+                    else{
+
+                        $toShow=array(
+
+                            'status' => 'error',
+                            'responseCode' => 404,
+                            'message'=>'couldn´t update the product'
+                        );
+                    }
+
+        
+                    echo json_encode($toShow);
+        
+                    $conn->closeConnectionPDO();
+ 
+
+                    
+                }
+            
+
+
+
+          
+
+
+        }
+        catch(PDOException $e){
+    
+            echo "update failed: " . $e->getMessage();
+
+        }
+    }
+
+    //subir imagen
+
+    public function uploadFile()
+    {
+        if(isset($_FILES['imagen']))
+        {
+            //echo "existe el archivo";
+
+            $piramideUploader=new PiramideUploader();
+
+            $response=$piramideUploader->upload("image-curso","imagen","assets/uploads/images",array("image/jpeg","image/png","image/gif"));
+        
+            $fileInfo=$piramideUploader->getInfoFile();
+
+            //echo $fileInfo["name"];
+
+           
+
+            //echo json_encode($fileInfo);
+
+            return $fileInfo; //array with name and everything, we will use the name to update database, passing that name to the updateFile method
+        
+        }
+        else
+        {
+            $toShow= array('status' => 'FAIL' ,
+                            'responseCode' => 404,
+                            'messagee' => 'no file uploaded'
+        
+                            );
+
+            echo json_encode($toShow);
+        }
+    }
+
+}
+
+
+?>
